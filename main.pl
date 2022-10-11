0a8a759ad84a27c03fa29675d3ab5f4d42e75f31
Klefur <joaquinalberto55@gmail.com>
2022-10-11 15:43:09 -0300

    se cambia rgb a hex por funciones no importadas

--- main.pl
+++ main.pl
@@ -1,25 +1,25 @@
-pixbit-d(X,Y,Bit,D,[X,Y,Bit,D]) :-
-    integer(X), integer(Y), integer(Bit), integer(D),
+pixbit-d(X,Y,Bit,Depth,[X,Y,Bit,Depth]) :-
+    integer(X), integer(Y), integer(Bit), integer(Depth),
     between(0, 1, Bit).
 
-pixrgb-d(X,Y,R,G,B,D,[X,Y,[R,G,B],D]) :-
-    integer(X), integer(Y), integer(R), integer(G), integer(B), integer(D),
+pixrgb-d(X,Y,R,G,B,Depth,[X,Y,[R,G,B],Depth]) :-
+    integer(X), integer(Y), integer(R), integer(G), integer(B), integer(Depth),
     between(0, 255, R), between(0, 255, G), between(0, 255, B).
 
-pixhex-d(X,Y,Hex,D,[X, Y, Hex, D]) :-
-    integer(X), integer(Y), string(Hex), integer(D),
+pixhex-d(X,Y,Hex,Depth,[X, Y, Hex, D]) :-
+    integer(X), integer(Y), string(Hex), integer(Depth),
     string_length(Hex,7).
 
-pixel(X,Y,Color,D,[X,Y,Color,D]) :-
-    pixbit-d(X,Y,Color,D,[X,Y,Color,D]);
-    pixhex-d(X,Y,Color,D,[X,Y,Color,D]);
-	pixrgb-d(X,Y,_,_,_,D,[X,Y,Color,D]).
+pixel(X,Y,Color,Depth,[X,Y,Color,Depth]) :-
+    pixbit-d(X,Y,Color,Depth,[X,Y,Color,Depth]);
+    pixhex-d(X,Y,Color,Depth,[X,Y,Color,Depth]);
+	pixrgb-d(X,Y,_,_,_,Depth,[X,Y,Color,Depth]).
 
-image(A,L,Pixeles,[A,L,Pixeles,C]):-
-    string(C);integer(C);is_list(C); C is -1.
+image(A,L,Pixeles,[Ancho,Alto,Pixeles,CompressColor]):-
+    string(CompressColor);integer(CompressColor);is_list(CompressColor); CompressColor is -1.
 
-setCompressValue([A,L,P,C], X, [A,L,P,X]) :-
-    image(_,_,_,[A,L,P,C]), image(_,_,_,[A,L,P,X]).
+setCompressValue([Ancho,Alto,Pixs,CompressColor], X, [Ancho,Alto,Pixs,X]) :-
+    image(_,_,_,[Ancho,Alto,Pixs,CompressColor]), image(_,_,_,[Ancho,Alto,Pixs,X]).
 
 imageIsBitmap([_,_,[],_]).
 imageIsBitmap([_,_,[H|T],_]):-
@@ -36,42 +36,42 @@ imageIsHexmap([_,_,[H|T],_]):-
     pixhex-d(_,_,_,_,H),
     imageIsHexmap([_,_,T,_]).
 
-imageIsCompressed([L,A,P,C]):-
-    C \== -1, image(_,_,_,[L,A,P,C]). 
+imageIsCompressed([Ancho,Alto,Pixs,CompressColor]):-
+    CompressColor \== -1, image(_,_,_,[Alto,Ancho,Pixs,CompressColor]). 
 
 imagen([_,_,[H|T],_]):-
     imageIsBitmap([_,_,[H|T],_]);imageIsPixmap([_,_,[H|T],_]);imageIsHexmap([_,_,[H|T],_]).
     
-imageFlipV([A,L,P,C],[A,L,P2,C]):-
-    image(_,_,_,[A,L,P,C]),
-    not(imageIsCompressed([A,L,P,C])),
-    flipPixsV(P,L,P2).
+imageFlipV([Ancho,Alto,Pixs,CompressColor],[Ancho,Alto,P2,CompressColor]):-
+    image(_,_,_,[Ancho,Alto,Pixs,CompressColor]),
+    not(imageIsCompressed([Ancho,Alto,Pixs,CompressColor])),
+    flipPixsV(Pixs,Alto,P2).
 
 flipPixsV([],_,[]).
-flipPixsV([H|T],L,[H2|T2]):-
-    pixel(X,Y,Color,D,H),
-    Y2 is L - 1 - Y,
-    pixel(X,Y2,Color,D,H2),
-    flipPixs(T,L,T2).
+flipPixsV([H|T],Alto,[H2|T2]):-
+    pixel(X,Y,Color,Depth,H),
+    Y2 is Alto - 1 - Y,
+    pixel(X,Y2,Color,Depth,H2),
+    flipPixs(T,Alto,T2).
 
-imageFlipH([A,L,P,C],[A,L,P2,C]):-
-    image(_,_,_,[A,L,P,C]),
-    not(imageIsCompressed([A,L,P,C])),
-    flipPixsH(P,A,P2).
+imageFlipH([Ancho,Alto,Pixs,CompressColor],[Ancho,Alto,P2,CompressColor]):-
+    image(_,_,_,[Ancho,Alto,Pixs,CompressColor]),
+    not(imageIsCompressed([Ancho,Alto,Pixs,CompressColor])),
+    flipPixsH(Pixs,A,P2).
 
 flipPixsH([],_,[]).
 flipPixsH([H|T],A,[H2|T2]):-
-    pixel(X,Y,Color,D,H),
+    pixel(X,Y,Color,Depth,H),
     X2 is A - 1 - X,
-    pixel(X2,Y,Color,D,H2),
+    pixel(X2,Y,Color,Depth,H2),
     flipPixsH(T,A,T2).
 
-imageCrop([A,L,P,C],X0,Y0,X1,Y1,I2) :-
-    image(_,_,_,[A,L,P,C]),
-    not(imageIsCompressed([A,L,P,C])),
+imageCrop([Ancho,Alto,Pixs,CompressColor],X0,Y0,X1,Y1,I2) :-
+    image(_,_,_,[Ancho,Alto,Pixs,CompressColor]),
+    not(imageIsCompressed([Ancho,Alto,Pixs,CompressColor])),
     cropPixs(P,X0,Y0,X1,Y1,P2),
-    A2 is X1 + 1 - X0, L2 is Y1 + 1 - Y0,
-    image(A2,L2,P2,I2).
+    Ancho2 is X1 + 1 - X0, Alto2 is Y1 + 1 - Y0,
+    image(Ancho2,Alto2,P2,I2).
 
 cropPixs([],_,_,_,_,[]).
 cropPixs([H|T],X0,Y0,X1,Y1,T2):-
@@ -79,37 +79,68 @@ cropPixs([H|T],X0,Y0,X1,Y1,T2):-
     (X =< X0;X1 =< X),(Y =< Y0; Y1 =< Y),
     cropPixs(T,X0,Y0,X1,Y1,T2).
 cropPixs([H|T],X0,Y0,X1,Y1,[H2|T2]):-
-    pixel(X,Y,Color,D,H),
+    pixel(X,Y,Color,Depth,H),
     X0 =< X,X =< X1,Y0 =< Y, Y =< Y1,
     X2 is X - X0, Y2 is Y - Y0,
-    pixel(X2,Y2,Color,D,H2),
+    pixel(X2,Y2,Color,Depth,H2),
     cropPixs(T,X0,Y0,X1,Y1,T2).
 
-imageRGBToHex([A,L,P,C], I2):-
-    imageIsPixmap([A,L,P,C]),
-    not(imageIsCompressed([A,L,P,C])),
-    rgbToHexPixs(P,P2),
-    image(A,L,P2,I2).
+imageRGBToHex([Ancho,Alto,Pixs,CompressColor], I2):-
+    imageIsPixmap([Ancho,Alto,Pixs,CompressColor]),
+    not(imageIsCompressed([Ancho,Alto,Pixs,CompressColor])),
+    rgbToHexPixs(Pixs,P2),
+    image(Ancho,Alto,P2,I2).
 
 rgbToHexPixs([],[]).
 rgbToHexPixs([H|T],[H2|T2]):-
-    pixrgb-d(X,Y,R,G,B,D,H),
-    hex_bytes(Hex,[R,G,B]), string_concat("#",Hex,Hex2),
-    pixhex-d(X,Y,Hex2,D,H2),
+    pixrgb-d(X,Y,R,G,B,Depth,H),
+    rgbToHex([R,G,B],Hex),
+    pixhex-d(X,Y,Hex,Depth,H2),
     rgbToHexPixs(T,T2).
 
-imageRotate90([A,L,P,C], I2):-
-    image(_,_,_,[A,L,P,C]),
-    not(imageIsCompressed([A,L,P,C])),
-    rotatePixs(P,L,P2),
-    image(L,A,P2,I2).
+hex("0",0).
+hex("1",1).
+hex("2",2).
+hex("3",3).
+hex("4",4).
+hex("5",5).
+hex("6",6).
+hex("7",7).
+hex("8",8).
+hex("9",9).
+hex("A",10).
+hex("B",11).
+hex("C",12).
+hex("D",13).
+hex("E",14).
+hex("F",15).
+
+rgbToHex([R,G,B], Hex):-
+	R1 is R // 16, R2 is R - (R1 * 16),
+    G1 is G // 16, G2 is G - (G1 * 16),
+    B1 is B // 16, B2 is B - (B1 * 16),
+    hex(StrR1, R1),hex(StrR2, R2),
+    string_concat(StrR1,StrR2,StrR3),
+    hex(StrG1, G1),hex(StrG2, G2),
+    string_concat(StrG1,StrG2,StrG3),
+    hex(StrB1, B1),hex(StrB2, B2),
+    string_concat(StrB1,StrB2,StrB3),
+    string_concat(StrR3,StrG3,StrRG3),
+    string_concat(StrRG3,StrB3,StrRGB),
+    string_concat("#",StrRGB,Hex).
+
+imageRotate90([Ancho,Alto,Pixs,CompressColor], I2):-
+    image(_,_,_,[Ancho,Alto,Pixs,CompressColor]),
+    not(imageIsCompressed([Ancho,Alto,Pixs,CompressColor])),
+    rotatePixs(P,Alto,P2),
+    image(Alto,A,P2,I2).
 
 rotatePixs([],_,[]).
-rotatePixs([H|T],L,[H2|T2]):-
-    pixel(X,Y,Color,D,H),
-    Y2 is L - 1 - Y,
-    pixel(Y2,X,Color,D,H2),
-    rotatePixs(T,L,T2).
+rotatePixs([H|T],Alto,[H2|T2]):-
+    pixel(X,Y,Color,Depth,H),
+    Y2 is Alto - 1 - Y,
+    pixel(Y2,X,Color,Depth,H2),
+    rotatePixs(T,Alto,T2).
 
 img1(I) :-
     pixbit-d(0, 0, 1, 10, P1),
