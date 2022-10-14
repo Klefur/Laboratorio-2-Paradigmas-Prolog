77449b82d3c842c7145f8455df565bff904e6745
Klefur <joaquinalberto55@gmail.com>
2022-10-14 12:53:54 -0300

    se agrega imageChangePixel

--- main.pl
+++ main.pl
@@ -37,13 +37,9 @@ imageIsHexmap([_,_,[H|T],_]):-
     imageIsHexmap([_,_,T,_]).
 
 imageIsCompressed([Width,Height,Pixs,CompressColor]):-
-    CompressColor \== -1, image(_,_,_,[Height,Width,Pixs,CompressColor]). 
-
-imagen([_,_,[H|T],_]):-
-    imageIsBitmap([_,_,[H|T],_]);imageIsPixmap([_,_,[H|T],_]);imageIsHexmap([_,_,[H|T],_]).
+    CompressColor \== -1, image(_,_,_,[Height,Width,Pixs,CompressColor]).
     
 imageFlipV([Width,Height,Pixs,CompressColor],[Width,Height,P2,CompressColor]):-
-    image(_,_,_,[Width,Height,Pixs,CompressColor]),
     not(imageIsCompressed([Width,Height,Pixs,CompressColor])),
     flipPixsV(Pixs,Height,P2).
 
@@ -55,7 +51,6 @@ flipPixsV([H|T],Height,[H2|T2]):-
     flipPixs(T,Height,T2).
 
 imageFlipH([Width,Height,Pixs,CompressColor],[Width,Height,P2,CompressColor]):-
-    image(_,_,_,[Width,Height,Pixs,CompressColor]),
     not(imageIsCompressed([Width,Height,Pixs,CompressColor])),
     flipPixsH(Pixs,Width,P2).
 
@@ -67,7 +62,6 @@ flipPixsH([H|T],Width,[H2|T2]):-
     flipPixsH(T,Width,T2).
 
 imageCrop([Width,Height,Pixs,CompressColor],X0,Y0,X1,Y1,I2) :-
-    image(_,_,_,[Width,Height,Pixs,CompressColor]),
     not(imageIsCompressed([Width,Height,Pixs,CompressColor])),
     cropPixs(Pixs,X0,Y0,X1,Y1,P2),
     Width2 is X1 + 1 - X0, Height2 is Y1 + 1 - Y0,
@@ -86,7 +80,6 @@ cropPixs([H|T],X0,Y0,X1,Y1,T2):-
     cropPixs(T,X0,Y0,X1,Y1,T2).
 
 imageRGBToHex([Width,Height,Pixs,CompressColor], I2):-
-    imageIsPixmap([Width,Height,Pixs,CompressColor]),
     not(imageIsCompressed([Width,Height,Pixs,CompressColor])),
     rgbToHexPixs(Pixs,P2),
     image(Width,Height,P2,I2).
@@ -100,7 +93,6 @@ rgbToHexPixs([H|T],[H2|T2]):-
     rgbToHexPixs(T,T2).
 
 imageToHistogram([Width,Height,Pixs,CompressColor], Histogram):-
-    image(_,_,_,[Width,Height,Pixs,CompressColor]),
     not(imageIsCompressed([Width,Height,Pixs,CompressColor])),
 	genHistogram(Pixs,Histogram).
 
@@ -137,7 +129,6 @@ getMayor(M,[Y,C],[[_,C2]|T]):-
     getMayor(M,[Y,C],T).
 
 imageRotate90([Width,Height,Pixs,CompressColor], I2):-
-    image(_,_,_,[Width,Height,Pixs,CompressColor]),
     not(imageIsCompressed([Width,Height,Pixs,CompressColor])),
     rotatePixs(Pixs,Height,P2),
     image(Height,Width,P2,I2).
@@ -149,10 +140,9 @@ rotatePixs([H|T],Height,[H2|T2]):-
     pixel(Y2,X,Color,Depth,H2),
     rotatePixs(T,Height,T2).
 
-imageCompress([Width,Height,Pixs,CompressValue],I3):-
-    image(_,_,_,[Width,Height,Pixs,CompressColor]),
-    not(imageIsCompressed([Width,Height,Pixs,CompressColor])),
-    imageToHistogram([Width,Height,Pixs,CompressValue], Histo),
+imageCompress([Width,Height,Pixs,CompressedColor],I3):-
+    not(imageIsCompressed([Width,Height,Pixs,CompressedColor])),
+    imageToHistogram([Width,Height,Pixs,CompressedColor], Histo),
     getMayor(M,[-1,-1],Histo),
     compressPixs(Pixs,M,P2),
     image(Width,Height,P2,I2),
@@ -166,23 +156,37 @@ compressPixs([H|T],M,[H|T2]):-
 compressPixs([_|T],M,T2):-
     compressPixs(T,M,T2).
 
+imageChangePixel([Width,Height,Pixs,CompressColor],P2Mod,I2):-
+    pixel(_,_,_,_,P2Mod),
+    not(imageIsCompressed([Width,Height,Pixs,CompressColor])),
+    changePixs(Pixs,P2Mod,Pixs2),
+    image(Width,Height,Pixs2,I2).
+
+changePixs([],_,[]).
+changePixs([H|T],PMod,[PMod|T2]):-
+    pixel(X,Y,_,_,H),
+    pixel(X,Y,_,_,PMod),
+    changePixs(T,PMod,T2).
+changePixs([H|T],PMod,[H|T2]):-
+    changePixs(T,PMod,T2).
+
 img1(I) :-
     pixbit-d(0, 0, 1, 10, P1),
-    pixbit-d(0, 1, 0, 10, P2),
+    pixbit-d(0, 1, 0, 12, P2),
     pixbit-d(1, 0, 0, 10, P3),
-    pixbit-d(1, 0, 1, 10, P4), 
+    pixbit-d(1, 0, 1, 12, P4), 
     image(2, 2, [P1, P2, P3, P4], I).
 
 img2(I) :-
     pixrgb-d(0, 0, 1, 10, 30, 10, P1),
-    pixrgb-d(0, 1, 0, 50, 132, 10, P2),
+    pixrgb-d(0, 1, 0, 50, 132, 12, P2),
     pixrgb-d(1, 0, 0, 70, 122, 10, P3),
-    pixrgb-d(1, 1, 1, 23, 133, 10, P4), 
+    pixrgb-d(1, 1, 1, 23, 133, 12, P4), 
     image(2, 2, [P1, P2, P3, P4], I).
 
 img3(I) :-
     pixhex-d(0, 0, "#192341", 10, P1),
-    pixhex-d(0, 1, "#00FFCC", 10, P2),
+    pixhex-d(0, 1, "#00FFCC", 12, P2),
     pixhex-d(1, 0, "#00FFDD", 10, P3),
-    pixhex-d(1, 0, "#77FFCC", 10, P4), 
+    pixhex-d(1, 0, "#77FFCC", 12, P4), 
     image(2, 2, [P1, P2, P3, P4], I).
